06d1380c425ba73da4f3c6b18201b7d189c8c486
WebGash Technologies <admin@travelops.com>
2026-03-07 12:57:39 +0530

    Cleanup: Remove debugging scripts containing echoes

--- backend/check_feature_count.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo App\Models\SubscriptionFeature::count();
--- backend/check_features.php
+++
@@ -1,5 +0,0 @@
-<?php
-$features = \App\Models\SubscriptionPlanFeature::all();
-foreach ($features as $f) {
-    fwrite(STDOUT, $f->key . PHP_EOL);
-}
